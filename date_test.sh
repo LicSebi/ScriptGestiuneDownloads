da8b2a6bcda1187149952e82477a242595e1df98
LicSebi <licuricisebi06@gmail.com>
2026-07-14 11:00:58 +0300

    generare mediu de lucru + date de test + curatare pentru retestare

---
+++ date_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "creare directoare"
+mkdir -p Downloads/Movies Downloads/Series Downloads/Music Media/Movies Media/Series Media/Music Media/Documents Media/Executables
+
+echo "creare fisiere test"
+
+# filme
+touch "Downloads/Movies/The.Matrix.1999.1080p.BluRay.x264.mkv"
+touch "Downloads/Movies/Inception (2010) [1080p] [BluRay] [YTS.MX].mp4"
+touch "Downloads/Movies/Pulp_Fiction_1994_720p_BRRip_XviD_AC3.avi"
+touch "Downloads/Movies/Interstellar.2014.IMAX.2160p.UHD.BluRay.HDR.HEVC.x265.mkv"
+touch "Downloads/Movies/Blade.Runner.2049.2017.1080p.WEB-DL.H264.m4v"
+
+# serial
+touch "Downloads/Series/Breaking.Bad.S05E14.1080p.BluRay.x264.mkv"
+touch "Downloads/Series/Friends_10x22_The_Last_One.avi"
+touch "Downloads/Series/The Office Season 3 Episode 23 Beach Games.mp4"
+touch "Downloads/Series/Sherlock.302.The.Sign.of.Three.720p.HDTV.mkv"
+touch "Downloads/Series/[Erai-raws] Shingeki no Kyojin - 75 [1080p][HEVC].mkv"
+
+# muzica
+touch "Downloads/Music/Michael Jackson - Billie Jean.mp3"
+touch "Downloads/Music/Daft.Punk.Get.Lucky.FLAC.flac"
+touch "Downloads/Music/The Weeknd feat. Daft Punk - Starboy.m4a"
+touch "Downloads/Music/David Guetta - Titanium (feat. Sia) (Radio Edit).mp3"
+touch "Downloads/Music/01. Queen - Bohemian Rhapsody.wav"
+
+# alte fisiere
+touch "Downloads/Curriculum Vitae - Licurici Sebastian.pdf"
+touch "Downloads/Proiect_Practica_v2.docx"
+touch "Downloads/burse_2026.xlsx"
+touch "Downloads/James-Clear-Atomic-Habits.epub"
+touch "Downloads/note_laborator.txt"
+touch "Downloads/PacketTracer_setup_v8.exe"
+touch "Downloads/git-installer-2.45.msi"
+touch "Downloads/wireshark-common_4.2.5_amd64.deb"
+touch "Downloads/backup_system.sh"
+touch "Downloads/discord-setup.dmg"
+
+echo "date test generate"
