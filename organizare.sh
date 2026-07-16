913176cff728332d00bf46159466e89bdb66b237
LicSebi <licuricisebi06@gmail.com>
2026-07-16 09:42:42 +0300

    procesare seriale + procesare muzica

--- organizare.sh
+++ organizare.sh
@@ -9,13 +9,13 @@ API_KEY="6c767dfb"
 pregatire_nume_film() {
 	local nume="$1"
 	
-	#stergere punct si underscore
+	#scapam de punct si underscore
 	local clean=$(echo "$nume" | tr '._' '  ')
 
-	#ramane doar numele, fara ani, rezolutii, formate etc
+	#pastram doar numele, fara ani, rezolutii, formate etc
 	clean=$(echo "$clean" | sed -E 's/([0-9]{4}|1080p|720p|2160p|4k|bluray|brrip|web-dl|hdtv|x264|x265|hevc|yts).*/ /I')
     
-	#elimin spatii si paranteze
+	#eliminam spatii si paranteze
 	clean=$(echo "$clean" | tr -d '[]()' | xargs)
     
 	echo "$clean"
@@ -34,10 +34,10 @@ proceseaza_film() {
     
 	echo "debug intergoare API $nume_curat"
     
-	#trimit cerere api, primesc json
+	#trimitem cerere api
 	local raspuns_json=$(curl -s "http://www.omdbapi.com/?apikey=${API_KEY}&t=$(echo "$nume_curat" | jq -sRr @uri)")
     
-	# verific daca filmul a fost gasit
+	# verificam daca filmul a fost gasit
 	local status=$(echo "$raspuns_json" | jq -r '.Response')
     
 	if [ "$status" = "True" ]; then
@@ -61,25 +61,120 @@ proceseaza_film() {
 	else
     
 		echo "!!! filmul '$nume_curat' nu a fost gasit"
-		
+		#mutam filmul intr un folder de erori
 		local dest_dir_necunoscut="$MEDIA_DIR/Movies/Uncategorized"
 		mkdir -p "$dest_dir_necunoscut"
 		mv "$file" "$dest_dir_necunoscut/"
-		
 	fi
 }
 
 
+
+
 proceseaza_serial() {
 	local file="$1"
-#	echo "debug serial $file"
-}
+	local filename=$(basename "$file")
+		
+	local extensie=$(echo "$filename" | rev | cut -d'.' -f1 | rev | tr '[:upper:]' '[:lower:]')
+		
+	local nume_curat=$(echo "$filename" | tr '._' '  ')
 
+	# Variabile pentru a stoca rezultatele extrase
+	local nume=""
+	local sezon=""
+	local episod=""
+
+	# TIPARUL 1: SXXEYY sau sXXeYY 
+	if [[ "$nume_curat" =~ ^(.*)[sS]([0-9]+)[eE]([0-9]+)(.*)$ ]]; then
+		nume=$(echo "${BASH_REMATCH[1]}" | xargs)
+		sezon=$(echo "${BASH_REMATCH[2]}" | xargs)
+		episod=$(echo "${BASH_REMATCH[3]}" | xargs)
+
+	# TIPARUL 2: formatul cu X 
+	elif [[ "$nume_curat" =~ ^(.*[^0-9])([0-9]+)[xX]([0-9]+)(.*)$ ]]; then
+		nume=$(echo "${BASH_REMATCH[1]}" | xargs)
+		sezon=$(echo "${BASH_REMATCH[2]}" | xargs)
+		episod=$(echo "${BASH_REMATCH[3]}" | xargs)
+
+	# TIPARUL 3: Season X Episode Y
+	elif [[ "$nume_curat" =~ ^(.*)[sS]eason[[:space:]]+([0-9]+)[[:space:]]+[eE]pisode[[:space:]]+([0-9]+)(.*)$ ]]; then
+		nume=$(echo "${BASH_REMATCH[1]}" | xargs)
+		sezon=$(echo "${BASH_REMATCH[2]}" | xargs)
+		episod=$(echo "${BASH_REMATCH[3]}" | xargs)
+
+	# TIPARUL 4: SSEE
+	# spatiu urmat de o cifra, dupa doua cifre, dupa spațiu
+	elif [[ "$nume_curat" =~ ^(.*)[[:space:]]([0-9])([0-9]{2})[[:space:]](.*)$ ]]; then
+		nume=$(echo "${BASH_REMATCH[1]}" | xargs)
+		sezon=$(echo "${BASH_REMATCH[2]}" | xargs)
+		episod=$(echo "${BASH_REMATCH[3]}" | xargs)
+
+	# TIPARUL 5: episod simplu, fara sezon
+	# sezon implicit 1
+	elif [[ "$nume_curat" =~ ^(.*)-[[:space:]]*([0-9]+)[[:space:]]*(.*)$ ]]; then
+		local nume_brut_anime=$(echo "${BASH_REMATCH[1]}" | sed -E 's/\[[^]]*\]//g' | xargs)
+		nume=$(echo "$nume_brut_anime" | sed 's/-//g' | xargs)
+		sezon="1"
+		episod=$(echo "${BASH_REMATCH[2]}" | xargs)
+	fi
+
+	if [ -n "$nume" ] && [ -n "$sezon" ]; then
+		# numerele ai intotdeauna 2 cifre
+		local sezon_padded=$(printf "%02d" "$sezon")
+		local episod_padded=$(printf "%02d" "$episod")
+
+		local dest_dir="$MEDIA_DIR/Series/$nume/Season $sezon_padded"
+		mkdir -p "$dest_dir"
+
+		# "Nume Serial - SXXEYY.extensie"
+		local noul_nume="${nume} - S${sezon_padded}E${episod_padded}.${extensie}"
+
+		echo "debug serial gasit $nume | Sezon $sezon_padded | Episod $episod_padded"
+		
+		mv "$file" "$dest_dir/$noul_nume"
+		    
+	else
+		
+		local dest_dir_uncategorized="$MEDIA_DIR/Series/Uncategorized"
+		mkdir -p "$dest_dir_uncategorized"
+		mv "$file" "$dest_dir_uncategorized/"
+		echo "debug pus in Uncategorized: $filename"
+	fi
+}
 
 proceseaza_muzica() {
 	local file="$1"
-#	echo "debug muzica $file"
+	local filename=$(basename "$file")
+	
+	local extensie=$(echo "$filename" | rev | cut -d'.' -f1 | rev | tr '[:upper:]' '[:lower:]')
+	local nume_fara_ext=$(echo "$filename" | rev | cut -d'.' -f2- | rev)
+	local nume_curat=$(echo "$nume_fara_ext" | tr '._' '  ' | xargs)
+
+	local dest_dir=""
+	local noul_nume=""
+
+	# verific separatorul standard -
+	if [[ "$nume_curat" =~ ^(.*)-[[:space:]]*(.*)$ ]]; then
+		# iau artist fara numarul de la inceput
+		local artist=$(echo "${BASH_REMATCH[1]}" | sed -E 's/^[0-9]+[[:space:]]*//' | xargs)
+		local titlu=$(echo "${BASH_REMATCH[2]}" | xargs)
+		
+		dest_dir="$MEDIA_DIR/Music/$artist"
+		noul_nume="${titlu}.${extensie}"
+		
+		echo "debug gasit muzica $artist - $titlu"
+	else
+		# fallback -> Unsure
+		dest_dir="$MEDIA_DIR/Music/Unsure"
+		noul_nume="${nume_curat}.${extensie}"
+		
+		echo "debug nerecunoscut $dest_dir/$noul_nume"
+	fi
+
+	mkdir -p "$dest_dir"
+	mv "$file" "$dest_dir/$noul_nume"
 }
+
 
 
 proceseaza_restul() {
@@ -87,7 +182,7 @@ proceseaza_restul() {
 	local filename=$(basename "$file") #doare nume, fara cale absoluta
 	local extensie=$(echo "$filename" | rev | cut -d'.' -f1 | rev | tr '[:upper:]' '[:lower:]') #extensia cu litere mici
 
-	if [ -x "$file" ]; #verific executabil
+	if [ -x "$file" ]; #verificam executabil
 		then
 
 		mv "$file" "$MEDIA_DIR/Executables/"
@@ -96,7 +191,7 @@ proceseaza_restul() {
 		return
 	fi
 
-#verific document sau alte executabile
+#verificam document sau alte executabile
 	case "$extensie" in
 	
 		pdf|odt|doc|docx|xls|xlsx|txt|epub)  # !!! adaug extensii pe parcurs daca e necesar !!!
@@ -126,11 +221,13 @@ find "$DOWNLOADS_DIR" -type f | while read -r file; do
 	case "$parent_dir" in
 	
 		"Movies")
-			proceseaza_film "$file"
+			#proceseaza_film "$file"
+			echo "debug film procesat"
 			;;
 			
 		"Series")
-			proceseaza_serial "$file"
+            #proceseaza_serial "$file"
+            echo "debug serial procesat"
             ;;
 
 		"Music")
@@ -138,7 +235,8 @@ find "$DOWNLOADS_DIR" -type f | while read -r file; do
 			;;
 
 		"Downloads")
-			proceseaza_restul "$file"
+			#proceseaza_restul "$file"
+			echo "debug altele procesat"
 			;;
 	esac
 done
